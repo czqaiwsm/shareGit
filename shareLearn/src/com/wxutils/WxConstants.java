9f86c0bed7e4fa84cd74216fee3a7c896e06b955
czq <1148392049@qq.com>
2016-09-01 00:21:33 +0800

    配件城项目代码

---
+++ shareLearn/src/com/wxutils/WxConstants.java
@@ -0,0 +1,21 @@
+package com.wxutils;
+
+public class WxConstants {
+
+    //appid
+    //请同时修改  androidmanifest.xml里面，.PayActivityd里的属性<data android:scheme="wxb4ba3c02aa476ea1"/>为新设置的appid
+    public static final   String APP_ID = "wxeb0d484726ebf84d";//wx2e2026024a6775af
+
+    //商户号
+    public static final String MCH_ID = "1382999902";
+
+    //API密钥，在商户平台设置
+    public static final String API_KEY="9463118e2cb4d4dacc4151abcccd34dd";
+
+    //分享
+//    public static final String APP_SECRET = "6f00df586628823728da4b0323cd61a8";
+
+    public static  String WX_NOTIFY_URL = "http://www.ienjoystudy.cn:8080/learn-interface/weixinPayServlet";
+
+
+}
